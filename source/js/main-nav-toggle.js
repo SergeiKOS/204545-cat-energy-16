87bd837a70196fad7f0f011fe91f5134e95c1f50
Sergei Kostrikin <cptlbc@yandex.ru>
2019-04-23 10:37:10 -0400

    Продолжение верстки

---
+++ source/js/main-nav-toggle.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var pageHeaderWrapper = document.querySelector('.page-header__wrapper');
+var mainNavToggle = document.querySelector('.main-nav__toggle');
+var siteList = document.querySelector('.site-list');
+
+pageHeaderWrapper.classList.remove('page-header__wrapper--nojs');
+
+mainNavToggle.addEventListener('click', function() {
+  if (mainNavToggle.classList.contains('main-nav__toggle--closed')) {
+    mainNavToggle.classList.remove('main-nav__toggle--closed');
+    siteList.style.display = 'none';
+  } else {
+    mainNavToggle.classList.add('main-nav__toggle--closed');
+    siteList.style.display = 'block';
+  }
+});
