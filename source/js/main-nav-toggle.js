ce060290124052d49cb917b724328c5beb2691e2
Sergei Kostrikin <cptlbc@yandex.ru>
2021-01-16 09:58:13 -0500

    Delete console.log and file rename

--- source/js/main-nav-toggle.js
+++ source/js/main-nav-toggle.js
@@ -1,20 +1,19 @@
-'use strict';
+"use strict";
 
-var pageHeaderWrapper = document.querySelector('.page-header__wrapper');
-var mainNavToggle = document.querySelector('.main-nav__toggle');
-var siteList = document.querySelector('.site-list');
+var pageHeaderWrapper = document.querySelector(".page-header__wrapper");
+var mainNavToggle = document.querySelector(".main-nav__toggle");
+var siteList = document.querySelector(".site-list");
 
-pageHeaderWrapper.classList.remove('page-header__wrapper--nojs');
+pageHeaderWrapper.classList.remove("page-header__wrapper--nojs");
 
-mainNavToggle.addEventListener('click', function() {
-  console.log('clicked');
-  if (mainNavToggle.classList.contains('main-nav__toggle--closed')) {
-    mainNavToggle.classList.remove('main-nav__toggle--closed');
-    siteList.style.display = 'none';
+mainNavToggle.addEventListener("click", function () {
+  if (mainNavToggle.classList.contains("main-nav__toggle--closed")) {
+    mainNavToggle.classList.remove("main-nav__toggle--closed");
+    siteList.style.display = "none";
   } else {
-    mainNavToggle.classList.add('main-nav__toggle--closed');
-    siteList.style.display = 'block';
-    siteList.style.position = 'static';
+    mainNavToggle.classList.add("main-nav__toggle--closed");
+    siteList.style.display = "block";
+    siteList.style.position = "static";
     pageHeaderWrapper.style.display = "block";
   }
 });
